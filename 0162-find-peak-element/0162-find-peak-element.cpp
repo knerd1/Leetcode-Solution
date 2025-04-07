0607c3abf880419b2d23d843da70afca711e112e
Ashutosh <55662919+knerd1@users.noreply.github.com>
2025-04-07 12:17:57 +0530

    0162-FIND-PEAK-ELEMENT Stats: Time: 0 ms (100%), Space: 12.5 MB (67.89%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -13,11 +13,8 @@ public:
         if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]) {return mid;
         }else if(nums[mid]>nums[mid-1]){
             low = mid +1;
-        } else if (nums[mid]>nums[mid+1]){
+        } else{
             high = mid-1;
-         }
-         else{
-            low= mid+1;
          }
          }
        return -1;
